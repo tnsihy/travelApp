fecef720dd519242209941923c930beb5896206d
tnsihy <tnsihy@foxmail.com>
2019-07-16 20:18:55 +0800

    add city-search-logic

--- travel/README.md
+++ travel/README.md
@@ -131,3 +131,9 @@ For a detailed explanation on how things work, check out the [guide](http://vuej
     - 使用`updated`计算一些值，当axios获取到数据时，cities才有数据，Alphabet.vue发生变化，里边的数据也会更新即触发`updated`
     - 屏幕滑动频率很高，解决：使用`setTimeout`去延迟执行
       - 在16ms之间又做了手指滑动，会把上一次操作清除再执行本次操作
+
+- 城市搜索框
+  - `v-show`
+  - 通过`stringObject.indexOf(searchvalue,fromindex) > -1`知道是否searchvalue存在
+  - 监听某个值的变化可以用`watch:{ ***(被监听的值):function(){}}`
+  - 尽量不要在模块中写逻辑 例如`v-show="!list.length"`
--- travel/src/components/city/Search.vue
+++ travel/src/components/city/Search.vue
@@ -1,28 +1,96 @@
 <template>
+  <div>
     <div class="search">
-        <input class="search-input" type="text" placeholder="输入城市名或拼音">
+      <input class="search-input" type="text" placeholder="输入城市名或拼音" v-model="keyword"/>
     </div>
+    <div class="search-content" ref="search" v-show="keyword">
+        <ul>
+            <li
+                class="search-item border-bottom"
+                v-for="item of list"
+                :key="item.id"
+            >{{item.name}}</li>
+            <li class="search-item border-bottom" v-show="hasNoData">
+                没有找到匹配数据
+            </li>
+        </ul>
+    </div>
+  </div>
 </template>
 
 <script>
+import BetterScroll from 'better-scroll'
 export default {
-  name: 'CitySearch'
+  name: 'CitySearch',
+  props: ['cities'],
+  data: function () {
+    return {
+      keyword: '',
+      list: [],
+      timer: null
+    }
+  },
+  watch: {
+    keyword: function () {
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      if (!this.keyword) {
+        this.list = []
+        return
+      }
+      this.timer = setTimeout(() => {
+        const result = []
+        for (let i in this.cities) {
+          this.cities[i].forEach((value) => {
+            if (value.spell.indexOf(this.keyword) > -1 || value.name.indexOf(this.keyword) > -1) {
+              result.push(value)
+            }
+          })
+        }
+        this.list = result
+      }, 100)
+    }
+  },
+  mounted: function () {
+    this.scroll = new BetterScroll(this.$refs.search)
+  },
+  computed: {
+    hasNoData: function () {
+      return !this.list.length
+    }
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
 @import '~styles/varibles.styl'
+
 .search
-    line-height .72rem
-    background-color $bgColor
-    padding .05rem .1rem
-    .search-input
-        line-height .62rem
-        height .62rem
-        border-radius .06rem
-        width 100%
-        text-indent .2rem
-        color #666
-        box-sizing border-box
-        padding 0 .1rem
+  line-height 0.72rem
+  background-color $bgColor
+  padding 0.05rem 0.1rem
+  .search-input
+    line-height 0.62rem
+    height 0.62rem
+    border-radius 0.06rem
+    width 100%
+    text-indent 0.2rem
+    color #666
+    box-sizing border-box
+    padding 0 0.1rem
+.search-content
+  z-index 1
+  background-color #eee
+  position absolute
+  left 0
+  right 0
+  bottom 0
+  top 1.68rem
+  .search-item
+    line-height .62rem
+    padding-left .2rem
+    background-color #fff
+    color #666
+
 </style>
